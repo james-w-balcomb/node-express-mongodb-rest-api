93aea4bde87a88bf4943d83aa1829b1da944f7ad
James Balcomb <james.w.balcomb@gmail.com>
2019-03-28 16:31:43 -0500

    Add CRUD End-Points added functions for {Create, Retrieve, Update, Delete} added routes for {Create, Retrieve, Update, Delete} added mongodb.ObjectID (needed to query by '_id') add middleware to process HTTP Request Message Content tested /, /create, /retrieve, /update, /delete

--- app.js
+++ app.js
@@ -2,8 +2,89 @@ const express = require('express')
 const app = express()
 const port = 3000
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectID
 
 const mongoConnection = MongoClient.connect('mongodb://localhost:27017/') // returns a Promise
+
+// https://nodejs.org/api/stream.html#stream_stream
+function streamConsumer(req, res, next) {
+  let body = '';
+  // Get the data as utf8 strings.
+  // If an encoding is not set, Buffer objects will be received.
+  req.setEncoding('utf8');
+  // Readable streams emit 'data' events once a listener is added
+  req.on('data', (chunk) => {
+    console.log(`Received ${chunk.length} bytes of data.`);
+    body += chunk;
+  });
+  // The 'end' event indicates that the entire body has been received
+  req.on('end', () => {
+    console.log('There will be no more data.');
+    try {
+      // const data = JSON.parse(body);
+      // // Write back something interesting to the user:
+      // res.write(typeof data);
+      // res.end();
+      let data = ''
+      if (req.headers['content-type'] === 'application/json') {
+        data = JSON.parse(body)
+      } else {
+        data = body
+      }
+      req.body = data
+      next()
+    } catch (er) {
+      // uh oh! bad json!
+      res.statusCode = 400;
+      return res.end(`error: ${er.message}`);
+    }
+  });
+}
+
+app.use(streamConsumer);
+
+/*
+  CRUD functions for MongoDB Documents (Database->Collection->Document)
+*/
+// (C)reate
+async function createDocument(doc) {
+  await mongoConnection.then(client => client.db('test').collection('test').insertOne(doc))
+}
+// (R)etrieve
+async function retrieveDocument(documentId) {
+  return mongoConnection.then(client => client.db('test').collection('test').findOne({"_id": new ObjectId(documentId)}))
+}
+// (U)pdate
+async function updateDocument(documentId, doc) {
+  delete doc._id
+  await mongoConnection.then(client => client.db('test').collection('test').updateOne({ _id: new ObjectId(documentId), },{$set: {...doc,},},))
+}
+// (D)elete
+async function deleteDocument(documentId) {
+  await mongoConnection.then(client => client.db('test').collection('test').deleteOne({"_id": new ObjectId(documentId)}))
+}
+/*
+  CRUD routes for API End-Points
+*/
+// (C)reate
+app.post('/create', async (req, res) => {
+  await createDocument(req.body)
+  res.send({ message: 'Created Document' })
+})
+// (R)etrieve
+app.get('/retrieve/:documentId', async (req, res) => {
+  res.send(await retrieveDocument(req.params.documentId))
+})
+// (U)pdate
+app.put('/update/:documentId', async (req, res) => {
+  await updateDocument(req.params.documentId, req.body)
+  res.send({ message: 'Updated Document' })
+})
+// (D)elete
+app.delete('/delete/:documentId', async (req, res) => {
+  await deleteDocument(req.params.documentId)
+  res.send({ message: 'Deleted Document' })
+})
 
 app.get('/', function(req, res) {
     mongoConnection.then(client => client.db('test').collection('test').find({}).toArray(function(err, docs) {
