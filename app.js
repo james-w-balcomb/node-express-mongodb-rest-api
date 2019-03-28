e3e91830306b044a10bfd62b03b602b926fef204
James Balcomb <james.w.balcomb@gmail.com>
2019-03-27 21:58:01 -0500

    Added MongoDB ran npm install --save mongodb added test data to local MongoDB server added new code for MongoDB Client, as a Promise ran node app.js visited http://127.0.0.1:3000/

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
+const MongoClient = require('mongodb').MongoClient
 
-app.get('/', (req, res) => res.send('hello, world'))
+const mongoConnection = MongoClient.connect('mongodb://localhost:27017/') // returns a Promise
+
+app.get('/', function(req, res) {
+    mongoConnection.then(client => client.db('test').collection('test').find({}).toArray(function(err, docs) {
+        if(err) { console.error(err) }
+        res.send(JSON.stringify(docs))
+    }))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
